daa20f0f390c8e29d183bb477cecd01d01b0e458
RichardLi8 <richard10m5n@outlook.com>
2023-03-01 02:22:52 -0500

    Updated Code

---
+++ InfiniteKnapsack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class InfiniteKnapsack{
+    public static void main(String[] args) throws IOException{
+        System.out.print("Enter the capacity: ");
+        int size = readInt();
+        System.out.print("Enter the number of items: ");
+        int n = readInt();
+        int dp [] = new int[size+1];
+        for(int i = 0; i < n; i++){
+            System.out.print("Enter the cost and value: ");
+            int c = readInt(), v = readInt();
+            for(int j = c; j <= size; j++){
+                dp[j] = Math.max(dp[j], dp[j-c] + v);
+            }
+        }
+        System.out.println("The max value is: " + dp[size]);
+    }
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static String next() throws IOException{
+        while(st == null || !st.hasMoreTokens()){
+            st = new StringTokenizer(br.readLine().trim());
+        }
+        return st.nextToken();
+    }
+    static int readInt() throws IOException{
+        return Integer.parseInt(next());
+    }
+}
